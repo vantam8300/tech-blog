c371c1c919e947475a8a9ad0649b57e424b5d351
vantam8300 <vantam8300@gmail.com>
2022-12-18 23:16:17 -0800

    finish logout function

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -34,21 +34,30 @@ router.post('/login', async (req, res) => {
         res.status(400).json(err);
     }
 });
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 router.post("/", async (req, res) => {
     try {
         console.log(req.body)
         const userData = await User.create(req.body);
-    
+
         req.session.save(() => {
-          req.session.user_id = userData.id;
-          req.session.logged_in = true;
-    
-          res.status(200).json(userData);
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
         });
-      } catch (err) {
+    } catch (err) {
         res.status(400).json(err);
-      }
+    }
 })
 
 module.exports = router;
