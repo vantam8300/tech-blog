16ee1e09638c0e7ab7b5abbe9b50e9e5a66c864e
vantam8300 <vantam8300@gmail.com>
2022-12-19 22:49:29 -0800

    finish adding and dispaly comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const auth = require("../utils/auth")
 const { Blog, User, Comment } = require('../models');
 
-
+// Get all blog
 router.get("/", async (req, res) => {
     try {
-        // Get all blog
+        
         const blogData = await Blog.findAll({
             include: [
                 {
@@ -23,14 +23,17 @@ router.get("/", async (req, res) => {
     }
 })
 
+// login
 router.get("/login", (req, res) => {
     res.render("login");
 })
 
+// sign up
 router.get("/signUp", (req, res) => {
     res.render("signUp");
 })
 
+// blog comments
 router.get("/blogs/:id", auth, async (req, res) => {
     try {
         const {id} = req.params;
@@ -46,12 +49,27 @@ router.get("/blogs/:id", auth, async (req, res) => {
                 {
                     model: Comment,
                     attributes: ['content', 'user_id', 'date_created'],
+                    
                 },
-              ],
+            ],
         })
         const blog = blogData.get({ plain: true });
-        console.log(blog)
-        res.render("comment", {blog, logged_in: req.session.logged_in });
+
+        const commentData = await Comment.findAll({
+            where: {
+                blog_id: id,
+            },
+            include: [
+                {
+                  model: User,
+                  attributes: ['name'],
+                },
+            ],
+        });
+
+        const comments = commentData.map(comment => comment.get({ plain: true }));
+        console.log(comments);
+        res.render("comment", {comments, blog, logged_in: req.session.logged_in });
     } catch (err) {
         console.log(err)
         res.status(500).json(err);
